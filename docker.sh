63bdcc4c35eb1f68d99b50f8ad352152b790435f
admitzaytsev <admitzaytsev@sberbank.ru>
2023-04-05 18:55:24 +0700

    os + helm

--- docker.sh
+++ docker.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 /Users/19208093/kka/tools/ApacheMaven3.6/bin/mvn clean package -T 1C -P dev_zad &&
-docker build -t addressbook/main:latest addressbook_main/. &&
-docker build -t addressbook/auth:latest addressbook_auth/. &&
-docker build -t addressbook/calc:latest addressbook_ageCalcFunction/.
+docker build -t andreyznsk/main:latest addressbook_main/. &&
+docker build -t andreyznsk/auth:latest addressbook_auth/. &&
+docker build -t andreyznsk/calc:latest addressbook_ageCalcFunction/.
+#docker push andreyznsk/main:latest &&
+#docker push andreyznsk/auth:latest &&
+#docker push andreyznsk/calc:latest
+
